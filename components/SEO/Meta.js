21d226edccc3de76170665228759767a252676d4
Sohit Vishwakarma <sohit777ma@gmail.com>
2023-11-21 17:34:49 +0530

    first commit

---
+++ components/SEO/Meta.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import Head from 'next/head';
+
+export default function Meta() {
+    return (
+        <Head>
+           /* Primary Meta Tags */
+            <title>Sohit Vishwakarma </title>
+            <meta charSet="utf-8" />
+            <meta name="title" content="Sohit vishwakarma Portfolio - Computer Engineering graduate" />
+            <meta name="description"
+                content="sohit vishwakarma's () Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="author" content="sohit vishwakarma (sohit)" />
+            <meta name="keywords"
+                content="sohit, sohit's portfolio, sohit linux, ubuntu portfolio, sohit vishwakarma protfolio,sohit vishwakarma computer, sohit vishwakarma, sohit ubuntu, sohit vishwakarma ubuntu portfolio" />
+            <meta name="robots" content="index, follow" />
+            <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+            <meta name="language" content="English" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="theme-color" content="#E95420" />
+
+            /* Search Engine */
+            <meta name="image" content="images/logos/fevicon.png" />
+            /* Schema.org for Google */
+            <meta itemProp="name" content="sohit vishwakarma Portfolio - Computer Engineering graduate" />
+            <meta itemProp="description"
+                content="sohit vishwakarma's (sohit) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta itemProp="image" content="images/logos/fevicon.png" />
+            /* Twitter */
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:title" content="sohit vishwakarma Portfolio - Computer Engineering graduate" />
+            <meta name="twitter:description"
+                content="sohit vishwakarma's (sohit) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="twitter:site" content="sohit" />
+            <meta name="twitter:creator" content="sohit" />
+            <meta name="twitter:image:src" content="images/logos/logo_1024.png" />
+            /* Open Graph general (Facebook, Pinterest & Google+) */
+            <meta name="og:title" content="sohit vishwakarma Portfolio - Computer Engineering graduate" />
+            <meta name="og:description"
+                content="sohit vishwakarma's (sohit) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="og:image" content="images/logos/logo_1200.png" />
+            <meta name="og:url" content="http://sohit.github.io/" />
+            <meta name="og:site_name" content="sohit vishwakarma Personal Portfolio" />
+            <meta name="og:locale" content="en_IN" />
+            <meta name="og:type" content="website" />
+
+            <link rel="icon" href="images/logos/fevicon.svg" />
+            <link rel="apple-touch-icon" href="images/logos/logo.png" />
+            <link rel="preload" href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" as="style" />
+            <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet"></link>
+        </Head>
+    )
+}
